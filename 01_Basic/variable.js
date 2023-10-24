a3ee4d184bbce6a5df21e5f5ed536402eb2d7e82
Arpit Bajpai <133994052+Appi169@users.noreply.github.com>
2023-10-24 11:02:59 +0000

    variable learn

---
+++ 01_Basic/variable.js
@@ -0,0 +1,17 @@
+const accountId = 114552;
+let accountName = "arpit"
+ accountCity= "nagpur"
+var accountEmail = "arpitbajpai163@gmail.com"
+let accountPassword = "Abaj@1235"
+
+// accountId = 2 // not allowed
+
+console.log(accountId);
+
+
+accountCity = "Bhopal"
+accountEmail = "habagj@g"
+accountPassword = "Bajpai@36"
+accountName = "anil"
+
+console.table([accountId,accountName,accountEmail,accountPassword,accountCity]);
