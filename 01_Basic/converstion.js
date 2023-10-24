7cc5819ddbea4a7686bfc1bb1f58a6b13de1219e
Arpit Bajpai <133994052+Appi169@users.noreply.github.com>
2023-10-24 21:17:20 +0000

    converstion

---
+++ 01_Basic/converstion.js
@@ -0,0 +1,19 @@
+// let score = "33abc"
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber); // number
+// console.log( valueInNumber); // NaN // not a number
+
+let score = null
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber); // number
+console.log( valueInNumber); // NaN // not a number
+
+let scores = 1
+
+let isBoolean = Boolean(scores)
+console.log(typeof isBoolean);
+console.log(isBoolean);
+
+
